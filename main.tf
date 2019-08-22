d6be42d2f11109f52759147412c3ac8893a28101
manjeet-github <manjeet@hashicorp.com>
2019-08-22 12:43:05 -0400

    files folder

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = "${var.location}"
 }
+
+#-- From private Module Registry
+
+module "web_app_container" {
+  source = "app.terraform.io/YOURORGNAME/web-app-container/azurerm"
+  name = "${var.prefix}"
+  port = "80"
+  htpps_only = "false"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  container_type = "docker"
+  container_image = "scarolan/palacearcade"
+}
+
+output "container_app_url" {
+  value = "http://${module.web_app_container.hostname}"
+}
